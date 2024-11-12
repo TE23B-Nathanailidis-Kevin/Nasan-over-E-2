e615718835fc4fc279b2fe5b3b6132727b9a5eb6
Kevin Nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2024-11-12 13:22:35 +0100

    Näsan över E 2

---
+++ Nasan-over-E-2/Program.cs
@@ -0,0 +1,12 @@
+﻿string [] sak = new string [6];
+sak[1] = "Glas";
+sak[2] = "Platta";
+sak[3] = "Träd";
+sak[4] = "Fluga";
+sak[5] = "Fågel";
+
+for (int i = 0; i < sak.Length; i++)
+{
+    Console.WriteLine( sak[i] ); 
+}
+Console.ReadLine();
