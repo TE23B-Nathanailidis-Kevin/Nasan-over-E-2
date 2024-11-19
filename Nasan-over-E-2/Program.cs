45ebc7f35bd1b58ba720e2c8ae009bb7b76ccd08
Kevin Nathanailidis <kevin.nathanailidis@elev.ga.ntig.se>
2024-11-19 13:20:42 +0100

    Progress Näsan 2

--- Nasan-over-E-2/Program.cs
+++ Nasan-over-E-2/Program.cs
@@ -1,12 +1,17 @@
-﻿string [] sak = new string [6];
-sak[1] = "Glas";
-sak[2] = "Platta";
-sak[3] = "Träd";
-sak[4] = "Fluga";
-sak[5] = "Fågel";
+﻿string [] sak = ["Glas", "Platta", "Träd", "Fluga", "Fågel"];
 
-for (int i = 0; i < sak.Length; i++)
-{
-    Console.WriteLine( sak[i] ); 
-}
+
+
+
+// new string [6];
+// sak[1] = "Glas";
+// sak[2] = "Platta";
+// sak[3] = "Träd";
+// sak[4] = "Fluga";
+// sak[5] = "Fågel";
+
+// for (int i = 0; i < sak.Length; i++)
+// {
+//     Console.WriteLine( sak[i] ); 
+// }
 Console.ReadLine();
